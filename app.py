c1edfddf6e0a3f68659bc148bc52c63938541858
MarinaMortg <marina.nikiforova@itcontact.ru>
2026-05-12 12:24:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,64 @@
 import streamlit as st
  
-st.title("*General English Checklist*")
+st.title("*IT English Level Check*")
+ 
  
 #Answers#
-st.subheader("Understanding/Comprehension")
+st.subheader("1.Understanding")
 understanding_answer = st.radio(
-   "",
-    ["переспрашивает, отвечает не по теме","теряется, понимает после повторения или переформулировки","понимает с первого раза, отвечает по сути"], 
-    index = 1 
+    "", 
+    ["Понимает, о чем текст в целом", "Понимает частично/с подсказками", "Не понимает"],
+index=1
 )
  
- 
-st.subheader("Fluency&Tempo of Speech")
-fluency_tempo_answer = st.radio(
+st.subheader("2.Details")
+details_answer = st.radio(
     "",
-    ["длинные частые паузы", "есть паузы, речь медленная, но связная", "комфортный темп, речь плавная"],
-    index = 1 
+    ["Верно отвечает на вопросы по тексту", "Отвечает неточно", "Не может ответить"],
+    index=1
 )
- 
 st.subheader("Confidence")
 confidence_answer = st.radio(
-    "", 
-    ["боится говорить","теряется при ошибках/может перейти на русский", "продолжает говорить, сам/а исправляется"],
-    index = 1 
+    "",
+    ["Читает отлично или с небольшими ошибками", "Читает с ошибками", "Сильно теряется"],
+    index=1
 )
  
-st.subheader("Pronunciation/Intelligibility")
-pronunciation_answer = st.radio(
-    "",
-    ["часто непонятно, мешает пониманию","в целом понятно","легко понятно"],
-   index = 1
+#Scores#
+Understanding_Scores = {
+    "Понимает, о чем текст в целом": 2,
+    "Понимает частично/с подсказками": 1,
+    "Не понимает": 0,
+}
+ 
+Details_Scores = {
+    "Верно отвечает на вопросы по тексту": 2,
+    "Отвечает неточно": 1,
+    "Не может ответить": 0,
+}
+ 
+Confidence_Scores = {
+    "Читает отлично или с небольшими ошибками": 2,
+    "Читает с ошибками": 1,
+    "Сильно теряется": 0
+}
+ 
+#Scores2#
+total_score = (
+    Understanding_Scores[understanding_answer] +
+    Details_Scores[details_answer] +
+    Confidence_Scores[confidence_answer]
 )
  
-st.subheader("Coherence/Structure of Answers")
-coherence_answer = st.radio(
-    "",
-    ["фразы обрывочные, нет логики", "слабая структура, перескакивает, но мысль понятна","лоигчно и последовательно"],
-index = 1 
-)
-st.subheader("Grammar Accuracy")
-grammar_answer = st.radio(
-    "",
-    ["ошибок много, мешают пониманию","ошибки есть, но не являются значительными", "редкие, незначительные ошибки/ошибок нет"],
-)
+def get_result_by_score(score):
+    if score < 3:
+        return "❌ Not OK"
+    elif score < 6:
+        return "⚠️ Partially OK"
+    else:
+        return "✅ Excellent"
+#Result#
+if st.button("Показать результат"):
+    st.success(get_result_by_score(total_score))
+    st.write("Сумма баллов:", total_score)
+
